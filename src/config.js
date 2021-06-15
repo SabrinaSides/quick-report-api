f8fafe09268e468b7bde56cef0da6a7f71fcf753
Sabrina Sides <sabrinasides91@gmail.com>
2021-06-15 14:01:51 -0500

    adding migration of database tables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://sabrina@localhost/quickreport',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://sabrina@localhost/quickreport_test'
 }
